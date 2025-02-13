f31b10a742f8f8914b85e6c679b06da670d03ce2
int0214 <jaeyuljung0@gmail.com>
2025-02-13 14:33:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const canvas = document.getElementById('gameCanvas');
-const context = canvas.getContext('2d');
-
-// Set replicanti count to 1
 document.getElementById('replicanti-count').innerText = 1;
-
-// Game logic here
