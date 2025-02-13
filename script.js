2bfbfadebcfa0129ae7338863ed93257b42ba508
int0214 <jaeyuljung0@gmail.com>
2025-02-13 14:29:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,4 @@ const context = canvas.getContext('2d');
 // Set replicanti count to 1
 document.getElementById('replicanti-count').innerText = 1;
 
-// Example: draw a rectangle
-context.fillStyle = 'green';
-context.fillRect(10, 10, 100, 100);
-
 // Game logic here
