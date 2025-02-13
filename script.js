398bc611cd563a2646e5becc2f7d83d21860860b
int0214 <jaeyuljung0@gmail.com>
2025-02-13 15:22:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ document.getElementById('replicanti-count').innerText = 1;
 
 document.getElementById('nerfs-button').addEventListener('click', function() {
     var nerfsContainer = document.getElementById('nerfs-container');
+    var nerfsButton = document.getElementById('nerfs-button');
+
     if (nerfsContainer.style.display === 'none') {
         nerfsContainer.style.display = 'block';
+        nerfsButton.innerText = 'Nerfs';
     } else {
         nerfsContainer.style.display = 'none';
+        nerfsButton.innerText = 'Nerfs';
     }
 });
