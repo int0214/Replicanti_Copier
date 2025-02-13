9053823a41274480e558ab29b2673a95d4dc0415
int0214 <jaeyuljung0@gmail.com>
2025-02-13 15:01:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-document.getElementById('replicanti-count').innerText = 1;
+body {
+    background-color: gray;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    justify-content: flex-start; /* Change from 'center' to 'flex-start' */
+}
+
+canvas {
+    display: none;
+}
+
+#replicanti-container {
+    font-size: 24px;
+    text-align: center;
+    margin-top: 20px;
+}
+
+#replicanti-count {
+    font-size: 32px;
+    font-weight: bold;
+}
+
+#nerfs-container {
+    margin-top: 20px;
+    text-align: center;
+    border: 1px solid black;
+    padding: 10px;
+    background-color: white;
+}
