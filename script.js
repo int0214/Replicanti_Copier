13da4568d8c302a5427e0b672b98cf2875b27068
int0214 <jaeyuljung0@gmail.com>
2025-02-13 14:17:16 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('gameCanvas');
+const context = canvas.getContext('2d');
+
+// Example: draw a rectangle
+context.fillStyle = 'green';
+context.fillRect(10, 10, 100, 100);
+
+// Game logic here
